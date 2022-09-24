8ba23078c01cbe11f7e9832b7350fda25bb80d8d
Fernando de Oliveira <ffernandodeoliveira@gmail.com>
2022-09-23 23:08:05 -0300

    Session 3 - done.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,13 +10,29 @@ import {
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
+interface SkillData {
+    id: string;
+    name: string;
+}
+
 export function Home() {
     const [newSkill, setNewSkill] = useState('');
-    const [mySkills, setMySkills] = useState([]);
+    const [mySkills, setMySkills] = useState<SkillData[]>([]);
     const [greeting, setGreeting] = useState('');
 
     function handleAddNewSkill() {
-        setMySkills(oldState => [...oldState, newSkill]);
+        const data = {
+            id: String(new Date().getTime()),
+            name: newSkill
+        }
+
+        setMySkills(oldState => [...oldState, data]);
+    }
+
+    function handleRemoveSkill(id: string) {
+        setMySkills(oldState => oldState.filter(
+            skill => skill.id !== id
+        ));
     }
 
     useEffect(() => {
@@ -24,7 +40,7 @@ export function Home() {
 
         if (currentHour < 12) {
             setGreeting('Good Morning');
-        } else if (currentHour >= 12 && greeting < 18) {
+        } else if (currentHour >= 12 && currentHour < 18) {
             setGreeting('Good Afternoon');
         } else {
             setGreeting('Good Night');
@@ -50,7 +66,10 @@ export function Home() {
                 onChangeText={setNewSkill}
             />
 
-            <Button onPress={handleAddNewSkill} />
+            <Button 
+                title='Add'
+                onPress={handleAddNewSkill}
+            />
 
             <Text style={[styles.title, { marginVertical: 20 }]}>
                 My Skill
@@ -58,9 +77,12 @@ export function Home() {
 
             <FlatList
                 data={mySkills}
-                keyExtractor={item => item}
+                keyExtractor={item => item.id}
                 renderItem={({ item }) => (
-                    <SkillCard skill={item} />
+                    <SkillCard 
+                        skill={item.name} 
+                        onPress={() => handleRemoveSkill(item.id)}
+                    />
                 )}
             />
 
@@ -72,9 +94,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1, 
         backgroundColor: '#121015',
-        paddingHorizontal: 20,
-        paddingVertical: 70,
         paddingHorizontal: 30,
+        paddingVertical: 70,
     },
     title: {
         color: '#FFF',
